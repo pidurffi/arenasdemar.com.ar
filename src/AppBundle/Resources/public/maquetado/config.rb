c00b024ad8c208b0fb11def8992ebf5253e24cba
Gabriel <gabriel@pc.galix.com.ar>
2018-09-24 14:07:45 -0300

    Subo maquetado

--- src/AppBundle/Resources/public/maquetado/config.rb
+++ src/AppBundle/Resources/public/maquetado/config.rb
@@ -3,10 +3,11 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
+css_dir = "css"
 sass_dir = "sass"
 images_dir = "images"
-javascripts_dir = "javascripts"
+javascripts_dir = "js"
+cache = false
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
