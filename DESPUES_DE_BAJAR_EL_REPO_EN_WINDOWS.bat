7a484e9596af3cbd94bb8fbc7a3485edf594e8a4
gabriel <gabriel@pc.galix.com.ar>
2018-09-24 14:43:55 -0300

    Espacio inicial

---
+++ DESPUES_DE_BAJAR_EL_REPO_EN_WINDOWS.bat
@@ -0,0 +1,5 @@
+cp vendor/almasaeed2010/adminlte/bootstrap public_html/theme
+cp vendor/almasaeed2010/adminlte/dist public_html/theme/dist
+cp vendor/almasaeed2010/adminlte/documentation public_html/theme/documentation
+cp vendor/almasaeed2010/adminlte/plugins public_html/theme/plugins
+cp vendor/almasaeed2010/adminlte/starter.html public_html/theme/starter.html
